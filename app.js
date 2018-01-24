bbbfb96ebc0c11123e62db279e0dc3f85612e60e
Rostyslaw Bornitskyy <doofixxx@gmail.com>
2018-01-24 21:32:29 +0200

    Delete public folder

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var index = require('./app-server/routes/index');
 
 var app = express();
 
@@ -15,7 +15,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
