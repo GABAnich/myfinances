fc087afdce610cb639ae0dfd88d847421d7d6b55
Rostyslaw Bornitskyy <doofixxx@gmail.com>
2018-01-29 21:36:01 +0200

    Add .eslintrc.json

--- api/controllers/hello_world.js
+++ api/controllers/hello_world.js
@@ -8,7 +8,7 @@
  Modules make it possible to import JavaScript files into your application.  Modules are imported
  using 'require' statements that give you a reference to the module.
 
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
+	It is a good idea to list the modules that your application depends on in the package.json in the project root
  */
 var util = require('util');
 
@@ -18,27 +18,27 @@ var util = require('util');
  For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
 
  Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
+	- The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
+	- Or the operationId associated with the operation in your Swagger document
 
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+	In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
+	we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
 module.exports = {
-  hello: hello
+	hello: hello
 };
 
 /*
-  Functions in a127 controllers used for operations should take two parameters:
+	Functions in a127 controllers used for operations should take two parameters:
 
-  Param 1: a handle to the request object
-  Param 2: a handle to the response object
+	Param 1: a handle to the request object
+	Param 2: a handle to the response object
  */
 function hello(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+	// variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+	var name = req.swagger.params.name.value || 'stranger';
+	var hello = util.format('Hello, %s!', name);
 
-  // this sends back a JSON response which is a single string
-  res.json(hello);
+	// this sends back a JSON response which is a single string
+	res.json(hello);
 }
--- api/controllers/homepage.js
+++ api/controllers/homepage.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 
 module.exports = {
-    index_get: index_get
-}
+	index_get: index_get
+};
 
 function index_get(req, res) {
-    res.set('Content-Type', 'text/html');
-    fs.readFile(__dirname + '../../../app_client/index.html', function(err, data) {
-        if (err) throw err;
+	res.set('Content-Type', 'text/html');
+	fs.readFile(__dirname + '../../../app_client/index.html', function(err, data) {
+		if (err) throw err;
 
-        res.write(data.toString('utf-8'));
-        res.end();
-    });
+		res.write(data.toString('utf-8'));
+		res.end();
+	});
 }
